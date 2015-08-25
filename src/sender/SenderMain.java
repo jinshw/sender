68c1dfd65513684609fa4abdbab6e5f6e9c61ee3
jinshw <jinshengwang2007@163.com>
2015-08-25 18:53:06 +0800

    文件复制

---
+++ src/sender/SenderMain.java
@@ -0,0 +1,34 @@
+package sender;
+
+import sender.monitor.ZJPFileListener;
+import sender.monitor.ZJPFileMonitor;
+
+public class SenderMain {
+
+	public static void main(String[] args) {
+		
+		
+		try {
+			//1、监控服务器发布文件夹，如果有新发布的作品，则打包复制到缓存文件夹中
+			ZJPFileMonitor m;
+			m = new ZJPFileMonitor(1000);
+			m.monitor("E:/filepath/code",new ZJPFileListener());
+			m.start();
+			
+			//2、监控缓存文件夹（待传输文件），如有文件进行socket接口传递
+//			ZJPFileMonitor zm;
+//			zm = new ZJPFileMonitor(1000);
+//			zm.monitor("E:/filepath/bak",new ZJPFileListener());
+//			zm.start();
+			
+			
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
